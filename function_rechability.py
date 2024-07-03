41d8c26ea0188aa198b95892de0bc62fff503ac9
root <root@WIN-47359G0BG44>
2024-07-03 18:28:02 +0900

    update

---
+++ function_rechability.py
@@ -0,0 +1,52 @@
+import idaapi
+import idautils
+
+def is_function_reachable(start_ea, target_ea):
+    start_func = idaapi.get_func(start_ea)
+    target_func = idaapi.get_func(target_ea)
+
+    def get_function_xrefs(func):
+        res = []
+        for head in idautils.Heads(func.start_ea, func.end_ea):
+            for xref in idautils.XrefsFrom(head, 0):
+                if xref.type in [idaapi.fl_CN, idaapi.fl_CF]:
+                    res.append(xref.to)
+        return res
+
+    visited = set()
+    
+    def dfs(current_func, path):
+        path.append(idaapi.get_func_name(current_func.start_ea))
+        if current_func.start_ea == target_func.start_ea:
+            return 2
+        
+        if current_func.start_ea in visited:
+            return 0
+        
+        visited.add(current_func.start_ea)
+        f = 0
+        for ea in get_function_xrefs(current_func):
+            called_func = idaapi.get_func(ea)
+            if called_func and dfs(called_func, path) == 2:
+                print("---TRACE---")
+                print(path[0])
+                for i in path[1:]:
+                    print(' -> '+i)
+                print()
+                f = 1
+            path.pop()
+        if f:
+            return 1
+        else:
+            return 0
+    
+    return dfs(start_func, [])
+
+start_function_name = "apic_mem_write"
+target_function_name = ".g_malloc"  
+target_ea = idaapi.get_name_ea(idaapi.BADADDR, target_function_name)
+start_ea = idaapi.get_name_ea(idaapi.BADADDR, start_function_name)
+if is_function_reachable(start_ea, target_ea):
+    print(f'{start_function_name} => {target_function_name} Reachable')
+else:
+    print(f'{start_function_name} => {target_function_name} Not Reachable')
